f4560d7462bfc9af6a5e3108e08fe88d2f445fed
dmitriyivashchenko <markfederal4@gmail.com>
2020-05-20 14:04:30 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let mixer = mixitup('.products__inner-items');
+  let mixer = mixitup('.products__inner-items');
+
+  $('.rate-stars').rateYo({
+      rating: 5,
+      starWidth: '12px',
+      readOnly: true,
+      ratedFill: '#ffa726'
+    });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  })
+
 });
