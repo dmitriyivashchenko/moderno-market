a1d5db577895b81d3496903677aec37a576765c3
dmitriyivashchenko <markfederal4@gmail.com>
2020-05-23 12:27:32 +0300

    added new-items.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let mixer = mixitup('.products__inner-items');
+
+  $('.js-range-slider').ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600
+  });
 
   $('.rate-stars').rateYo({
       rating: 5,
@@ -13,6 +20,21 @@ document.addEventListener('DOMContentLoaded', () => {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4
-  })
+  });
+
+  $('.icon-th-list').on('click', () => {
+    $('.products-page__items .product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+  $('.icon-th-large').on('click', () => {
+    $('.products-page__items .product__item').removeClass('list');
+    $('.icon-th-list').removeClass('active');
+    $('.icon-th-large').addClass('active');
+  });
+
+  let mixer = mixitup('.products__inner-items');
 
 });
+
